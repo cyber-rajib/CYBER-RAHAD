010132d70a3601e55d58518e931946647e1d1e80
SK.SIDDIK.KHAN <siddik21177190@gmail.com>
2024-09-10 12:05:13 +0600

    Update index.js

--- SIDDIK/system/login/index.js
+++ SIDDIK/system/login/index.js
@@ -3,7 +3,7 @@
 var utils = require("./utils");
 var cheerio = require("cheerio");
 var log = require('npmlog');
-const logger = require('../../catalogs/nazrulc.js');
+const logger = require('../../catalogs/siddikc.js');
 const chalk = require('chalk');
 var defaultLogRecordSize = 100;
 log.maxRecordSize = defaultLogRecordSize;
