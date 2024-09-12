9236c35e4b6e11073a5c5d0da8e8cfb8b11f14d5
SK.SIDDIK.KHAN <siddik21177190@gmail.com>
2024-09-12 10:57:46 +0600

    Update model.js

--- SIDDIK/system/database/model.js
+++ SIDDIK/system/database/model.js
@@ -1,14 +1,14 @@
 module.exports = function (input) {
 	const force = false;
-
+ 
 	const Users = require("./models/users.js")(input);
 	const Threads = require("./models/threads.js")(input);
 	const Currencies = require("./models/currencies.js")(input);
-
+ 
 	Users.sync({ force });
 	Threads.sync({ force });
 	Currencies.sync({ force });
-
+ 
 	return {
 		model: {
 			Users,
