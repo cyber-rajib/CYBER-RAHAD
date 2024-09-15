fd7d6c2a0b04b20127846cef6084b1ad9985082b
SK.SIDDIK.KHAN <siddik21177190@gmail.com>
2024-09-15 15:54:29 +0600

    Update 400 SIDDIK 10.js

--- scripts/commands/400 SIDDIK 10.js
+++ scripts/commands/400 SIDDIK 10.js
@@ -2,21 +2,23 @@ const fs = require("fs");
 module.exports = {
   config:{
   name: "siddik-10",
-  version: "1.0.1",
-  prefix: false,
+        version: "1.0.1",
+        prefix: false,
   permssion: 0,
   credits: "SIDDIK", 
-  description: "মানুষ_দুনিয়ার অতিথি মাত্র",
+  description: "মানুষ দুনিয়ার অতিথি মাত্র",
   category: "no prefix",
   usages: "siddik-10",
-  cooldowns: 5, 
+        cooldowns: 5, 
 },
  
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	if (event.body.indexOf("😥")==0 || event.body.indexOf("😥😥")==0 || event.body.indexOf("😥😥😥")==0 || event.body.indexOf("😥😥😥😥")==0) {
-		var msg = {
-				body: "╭─────•◈•─────╮\n         🅂🄸🄳🄳🄸🄺🄱🄾🅃       \n\n─•মানুষ_দুনিয়ার অতিথি মাত্র-😅🩷🪽🩵🪽\n\n         𝗦𝗞.𝗦𝗜𝗗𝗗𝗜𝗞.𝗞𝗛𝗔𝗡       \n╰─────•◈•─────╯",
+handleEvent: function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+  if (body.indexOf("😥")==0 || body.indexOf("😥😥")==0 || body.indexOf("😥😥😥")==0 || body.indexOf("😥😥😥😥")==0) {
+    var msg = {
+        body: "╭─────•◈•─────╮\n         🅂🄸🄳🄳🄸🄺🄱🄾🅃       \n\n─•মানুষ_দুনিয়ার অতিথি মাত্র-😅🩷🪽🩵🪽\n\n         𝗦𝗞.𝗦𝗜𝗗𝗗𝗜𝗞.𝗞𝗛𝗔𝗡       \n╰─────•◈•─────╯",
 				attachment: fs.createReadStream(__dirname + `/Siddik/siddik-10.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
