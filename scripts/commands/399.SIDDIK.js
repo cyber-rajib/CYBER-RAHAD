e26c5576ce5f5143b3c30857385c5f924b562505
SK.SIDDIK.KHAN <siddik21177190@gmail.com>
2024-09-15 19:38:41 +0600

    Create 399.SIDDIK.js

---
+++ scripts/commands/399.SIDDIK.js
@@ -0,0 +1,24 @@
+module.exports.config = {
+name: "sada",
+  version: "",
+  hasPermssion: 1,
+  credits: "SIDDIK",
+  description: "blast the bos in 1 sec",
+  commandCategory: "test",
+  usages: "addmin",
+  cooldowns: 5,
+  dependencies: "",
+};
+ 
+module.exports.run = function ({ api, event, Users }) {
+  var { threadID, messageID } = event;
+  var k = function (k) { api.sendMessage(k, threadID)};
+ 
+  //*vonglap
+ 
+for (i = 0; i < 400; i++) {
+ k("🅂🄺 🅂🄸🄳🄳🄸🄺 🄺🄷🄰🄽");
+}
+ 
+  }
+ 
