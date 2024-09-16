3dd6aa6cb215ea9f4dde6e9dc0def46b2226152e
SK.SIDDIK.KHAN <siddik21177190@gmail.com>
2024-09-17 01:21:00 +0600

    Update imgur.js

--- scripts/commands/imgur.js
+++ scripts/commands/imgur.js
@@ -80,7 +80,7 @@ const imgurLink = await uploadToImgur(attachment);
 
 console.log('Imgur link:', imgurLink);
 
-const replyMessage = ` 𝖨𝗆𝗀𝗎𝗋 𝗅𝗂𝗇𝗄: ${imgurLink}
+const replyMessage = ` ${imgurLink}
   `;
 
 return api.sendMessage({ body: replyMessage }, event.threadID, event.messageID);
