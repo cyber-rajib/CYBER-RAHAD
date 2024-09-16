52acb080d6e3e98e990404a3869eeae7e2ff627b
SK.SIDDIK.KHAN <siddik21177190@gmail.com>
2024-09-17 01:11:21 +0600

    Update imgur.js

--- scripts/commands/imgur.js
+++ scripts/commands/imgur.js
@@ -1,66 +1,40 @@
-const axios = require('axios');
-const fs = require('fs');
-const FormData = require('form-data');
-const url = require('url');
- 
 module.exports.config = {
-    name: "imgur2",
+    name: "imgur",
     version: "1.0.0",
     permission: 0,
-    credits: "Rahad",
+    credits: "SIDDIK",
     description: "Uploads replied attachment to Imgur",
     prefix: false, 
     category: "Video and images Imgur upload", 
-    usages: "imgur2",
+    usages: "imgur",
     cooldowns: 5,
     dependencies: {
         "axios": ""
     }
 };
  
-module.exports.run = async ({ api, event }) => {
-    try {
-        const attachmentUrl = event.messageReply.attachments[0]?.url || event.messageReply.attachments[0];
-        if (!attachmentUrl) return api.sendMessage('Please reply to an image or video with /imgur', event.threadID, event.messageID);
-        var { threadID, messageID, body } = event,
-            { PREFIX } = global.config;
-        let threadSetting = global.data.threadData.get(threadID) || {};
-        let prefix = threadSetting.PREFIX || PREFIX;
-      
-        const attachment = (await axios.get(attachmentUrl, { responseType: 'arraybuffer' })).data;
+module.exports.run = async function({ api, event, args }) {
+  const linkanh = event.messageReply.attachments[0].url || args.join(" ");
+    const axios = require("axios")
+    const request = require("request")
+    const fs = require("fs-extra")
+  var imgur = require('imgur-upload-api'),
+  path = require('path');
+  const myClientID = 'Client-ID 3fb071726880bbb'
+  imgur.setClientID(myClientID);
  
-        const imgurLink = await uploadToImgur(attachment);
- 
-        console.log('Imgur link:', imgurLink);
- 
-        const replyMessage = "${imgurLink}",
- 
-        return api.sendMessage({ body: replyMessage }, event.threadID, event.messageID);
-    } catch (error) {
-        console.error('Error:', error.response?.data || error.message);
-        return api.sendMessage('An error occurred while processing the attachment.', event.threadID, event.messageID);
+  imgur.upload(linkanh, function (err,res) {
+    console.log(res)
+    const link = res.data.link;
+    const type = res.data.type;
+    var msg = [];
+    {
+        msg += `TYPE: ${type}\nLINK: ${link}`
     }
-};
+    return api.sendMessage({
+        body: msg
  
-async function uploadToImgur(attachment) {
-    try {
-        const formData = new FormData();
-        formData.append('image', attachment);
+    }, event.threadID, event.messageID);
+  });
  
-        console.log('Uploading to Imgur...');
- 
-        const uploadResponse = await axios.post('https://api.imgur.com/3/upload', formData, {
-            headers: {
-                ...formData.getHeaders(),
-                Authorization: `Client-ID c76eb7edd1459f3` 
-            }
-        });
- 
-        console.log('Upload response:', uploadResponse.data);
- 
-        return uploadResponse.data.data.link;
-    } catch (error) {
-        console.error('Imgur upload error:', error.response?.data || error.message);
-        throw new Error('An error occurred while uploading to Imgur.');
-    }
-} 
+}
