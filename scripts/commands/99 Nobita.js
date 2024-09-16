6c502abc2a96143941a1bab7f30ddb7470b65d30
SK.SIDDIK.KHAN <siddik21177190@gmail.com>
2024-09-16 20:41:23 +0600

    Update and rename 100.Nobita.js to 99 Nobita.js

--- scripts/commands/99 Nobita.js
+++ scripts/commands/99 Nobita.js
@@ -1,6 +1,6 @@
 /** I am doing this coding with a lot of difficulty, please don't post it yourself¯\_(ツ)_/¯ **/
 module.exports.config = {
-	name: "nobita",
+  name: "nobita",
   version: "1.0.0",
   permission: 0,
   credits: "SIDDIK",
