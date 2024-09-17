d43b002bdcda9a0fc62e978191ff18e5fd106977
SK.SIDDIK.KHAN <siddik21177190@gmail.com>
2024-09-17 11:38:03 +0600

    Update গজল.js

--- scripts/commands/গজল.js
+++ scripts/commands/গজল.js
@@ -1,8 +1,8 @@
 module.exports.config = {
-	name: "gojol",
+  name: "gojol",
   version: "1.0.0",
   permission: 0,
-  credits: "Gm Nuruzzaman",
+  credits: "SIDDIK",
   description: "Made By Siddik",
   prefix: true, 
   category: "user", 
