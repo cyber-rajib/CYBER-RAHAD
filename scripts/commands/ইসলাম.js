29364efb10873ff0dc67d1240acf40e7992f0063
SK.SIDDIK.KHAN <siddik21177190@gmail.com>
2024-09-17 11:37:24 +0600

    Update ইসলাম.js

--- scripts/commands/ইসলাম.js
+++ scripts/commands/ইসলাম.js
@@ -1,6 +1,6 @@
 /** Đổi Credit ? Bọn t đã không mã hóa cho mà edit rồi thì tôn trọng nhau tý đi ¯\_(ツ)_/¯ **/
 module.exports.config = {
-	name: "ইসলাম",
+  name: "ইসলাম",
   version: "1.0.0",
   permission: 0,
   credits: "SIDDIK",
