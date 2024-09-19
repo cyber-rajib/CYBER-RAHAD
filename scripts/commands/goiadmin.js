dfb2108d218553970d24014ca969111fe84d37f3
SK.SIDDIK.KHAN <siddik21177190@gmail.com>
2024-09-19 10:44:44 +0600

    Update goiadmin.js

--- scripts/commands/goiadmin.js
+++ scripts/commands/goiadmin.js
@@ -1,16 +1,17 @@
 module.exports.config = {
-  name: "admintag",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "SIDDIK",
-  description: "Bot will reply if admin is tag in a message.",
-  commandCategory: "Other",
-  usages: "",
-  cooldowns: 1
+  name: "goiadmin",
+    version: "1.0.0",
+    permission: 0,
+    credits: "SIDDIK",
+    description: "mention",
+    prefix: true,
+    category: "user",
+    usages: "tag",
+    cooldowns: 5,
 };
 module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "100059026788061","100005002412625") {
-    var aid = ["100059026788061",100005002412625"];
+  if (event.senderID !== "100005002412625") {
+    var aid = ["100005002412625"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
       var msg = ["Don't Mention My Owner Busy Rights Now💞","আমার বস চিপায়  বিজি আছে___🌝", "মেয়ে পটাতে গেছে___😁", "এমন ভাবে মেনশান না দিয়ে একটা জি এফ দাও__🙈", "এত ডাকিস কেন__😡\n আমার বস অনেক বিজি__☺️", "বস কই তুমি\nতোমারে এক বলদে খোজ করে__🤣"];
