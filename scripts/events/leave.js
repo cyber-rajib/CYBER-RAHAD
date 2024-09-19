6296a20819cfe4d64f071c0034ee594723f2247f
SK.SIDDIK.KHAN <siddik21177190@gmail.com>
2024-09-19 08:39:16 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -17,7 +17,7 @@ module.exports.run = async function({ api, event, Users, Threads }) {
   const { threadID } = event;
   const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
   const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "  তোর সাহস কম নয় এখানে নজরুল এর ইসলামিক চ্যাট থাকতে তুই লিভ নিস😡😠🤬 \n✢━━━━━━━━━━━━━━━✢\n ----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖----" : "|────────> WELLCOME REMOVE🤧 ";
+  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "  তোর সাহস কম নয় এখানে নজরুল এর ইসলামিক চ্যাট থাকতে তুই লিভ নিস😡😠🤬 \n✢━━━━━━━━━━━━━━━✢\n ----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖----" : "তোমার এই গ্রুপে থাকার কোনো যোগ্যাতা নেই আবাল😡।\nতাই তোমার লাথি মেরে গ্রুপ থেকে বের করে দেওয়া হলো🤪। WELLCOME REMOVE🤧 \n✢━━━━━━━━━━━━━━━✢\n ----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖----";
   const path = join(__dirname, "Nazrul", "leaveGif");
   const gifPath = join(path, `leave1.gif`);
   var msg, formPush
@@ -32,3 +32,4 @@ module.exports.run = async function({ api, event, Users, Threads }) {
  
   return api.sendMessage(formPush, threadID);
 }
+ 
