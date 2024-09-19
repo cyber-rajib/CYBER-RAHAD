bc560e6c9914d7a16479ed87d14cf42bdcff5555
SK.SIDDIK.KHAN <siddik21177190@gmail.com>
2024-09-19 09:25:55 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "leave",
   eventType: ["log:unsubscribe"],
   version: "1.0.0",
-  credits: "nazrul",
+  credits: "SIDDIK",
   description: "Thông báo bot hoặc người rời khỏi nhóm",
   dependencies: {
     "fs-extra": "",
@@ -17,14 +17,14 @@ module.exports.run = async function({ api, event, Users, Threads }) {
   const { threadID } = event;
   const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
   const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "  ╰‣ Lift The Group" : "\n╰‣ WELLCOME REMOVE \n━━━━━━━━━━━━━";
-  const path = join(__dirname, "Nazrul", "leaveGif");
+  const type = (event.author == event.logMessageData.leftParticipantFbId) ? " left the group" : "WELLCOME REMOVE";
+  const path = join(__dirname, "Siddik", "leaveGif");
   const gifPath = join(path, `leave1.gif`);
   var msg, formPush
  
   if (existsSync(path)) mkdirSync(path, { recursive: true });
  
-  (typeof data.customLeave == "undefined") ? msg = "➴ {name} {type} " : msg = data.customLeave;
+  (typeof data.customLeave == "undefined") ? msg = " {name} {type} " : msg = data.customLeave;
   msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
  
   if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
