1492fd7c5b40ea1d1fc42c34349c9dbccbb39bed
RAHATSHIRIN <kr8088703@gmail.com>
2024-09-26 22:59:46 +0600

    Update autolink.js

--- scripts/commands/autolink.js
+++ scripts/commands/autolink.js
@@ -17,23 +17,24 @@ handleEvent: async function ({ api, event, args }) {
     const fs = require("fs-extra")
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  const {alldl} = require("rahad-all-downloader")
+  const {alldown} = require("nayan-media-downloader")
   if (body.startsWith("https://")) {
-  api.setMessageReaction("🌿", event.messageID, (err) => {}, true);
-const data = await alldl(content);
+  api.setMessageReaction("🔍", event.messageID, (err) => {}, true);
+const data = await alldown(content);
   console.log(data)
-  const {title, videoUrl} = data.data;
-    api.setMessageReaction("🦋", event.messageID, (err) => {}, true);
-  const video = (await axios.get(videoUrl, {
+  const {low, high, title} = data.data;
+    api.setMessageReaction("✔️", event.messageID, (err) => {}, true);
+  const video = (await axios.get(high, {
       responseType: "arraybuffer",
     })).data;
     fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
-
+ 
         return api.sendMessage({
-            body: `⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆\n\nᴛɪᴛʟᴇ ${title}\n\n⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆`,
+            body: `《TITLE》: ${title}`,
             attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
-
+ 
         }, event.threadID, event.messageID);
     }
 }
 }
+ 
