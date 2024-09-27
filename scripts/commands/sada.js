cf5f447380608c924eca140b291f40fc52f54cd9
RAHATSHIRIN <kr8088703@gmail.com>
2024-09-27 23:02:04 +0600

    Update and rename 50K_Hn.js to sada.js

---
+++ scripts/commands/sada.js
@@ -0,0 +1,24 @@
+module.exports.config = {
+  config:{
+  name: "sada",
+  version: "1.0.1",
+  prefix: true,
+  permssion: 2,
+  credits: "RAHAT", 
+  description: "Sada🌺",
+  category: "Khan Rahul RK",
+  usages: "Spam 💞",
+  cooldowns: 5, 
+},
+
+module.exports.run = function ({ api, event, Users }) {
+  var { threadID, messageID } = event;
+  var k = function (k) { api.sendMessage(k, threadID)};
+
+  //*vonglap
+
+for (i = 0; i < 400; i++) {
+ k("🅂🄾🅁🅁🅈");
+}
+
+  }
