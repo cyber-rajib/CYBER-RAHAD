6334689f0ec0b9156ece8b9fd97c2b69b8a22fe6
RAHATSHIRIN <kr8088703@gmail.com>
2024-09-28 22:51:43 +0600

    Update dallev2.js

--- scripts/commands/dallev2.js
+++ scripts/commands/dallev2.js
@@ -4,27 +4,25 @@ const path = require('path');
 
 module.exports.config = {
   name: "edit",
-  version: "1.0.0",
-  permission: 2,
+  version: "1.0.1",
   prefix: true,
-  credits: "RAHAT",
-  description: "download",
-  category: "Generate images by Dalle-3 AI",
-  usages: "[text] \nJamon [A 17/18/19 years old boy/girl watching football match on tv and written RAHAT and 69 on the back of his Dress , 4k]",
-  dependencies: {
-        "axios": "",
-        "fs-extra": ""
-  };
+  permssion: 2,
+  credits: "💞RAHAT💞", 
+  description: "Generate images by Dalle-3 AI",
+  category: "download",
+  usages: "[text] \nJamon [A 17/18/19 years old boy/girl watching football match on tv and written Rahul and 69 on the back of his Dress , 4k",
+  cooldowns: 5, 
+},
 
 module.exports.run = async function ({ api, event, args }) {
   const prompt = event.messageReply?.body.split("dalle")[1] ||  args.join(" ");
   if (!prompt) {
-   return api.sendMessage("❌| Wrong Formet .✅ | Use 17/18 years old boy/girl watching football match on tv and written RAHAT and 69 on the back of his Dress , 4k",event.threadID,event.messageID);
+   return api.sendMessage("❌| Wrong Formet .✅ | Use 17/18 years old boy/girl watching football match on tv and written RAHUL and 69 on the back of his Dress , 4k",event.threadID,event.messageID);
   }
     try {
       const w = await api.sendMessage("𝙥𝙡𝙚𝙖𝙨𝙚 𝙬𝙞𝙩𝙝𝙚 𝙥𝙧𝙤𝙘𝙚𝙨𝙨𝙞𝙣𝙜 𝙮𝙤𝙪𝙧 𝙞𝙢𝙖𝙜𝙚 \n\n𝙠𝙝𝙖𝙣 𝙧𝙖𝙝𝙪𝙡 𝙧𝙠💞", event.threadID);
-  
-const response = await axios.get(`https://www.noobs-api.000.pe/dipto/dalle?prompt=${prompt}&key=dipto008&cookies=1z3z4msw2_uRb2QQj82bWAy21qyRGzWfqlaqrw9FG_bmJuyVavj9muQs5gl-5AhyOmmsisOLQGJNpjB7nFrPmewrZi55-yOMOi9A8mET5kXXVV0zWfNcRlJPbxg5dqPZKpO2KYKNSx7934yXQtqBCf8Zc97O6bUHbKUGD6oETanwZKO1f1JDS4RRfrztteLodORno4nDI9IA_eotNWlEbtw`)
+
+const response = await axios.get(`https://www.noobs-api.000.pe/dipto/dalle?prompt=${prompt}&key=dipto008&cookies=1kbBnSXuYX4wUmbsffOeinDFhmu4fPrE_JU_vF8c5h6dzQDUDOLsMzY5ZUG19B1pwHbSoQTQOBIchVTxffBlfP_WF5iDq30jjagUnxLNrWQzpFidW3Yqmo7uPY-bmSKk-GrZfAB7REZz-hvXQPgKEJfS4zo3zh52-yPlAn_a-7UGxf2XITeYov07E2ZkDbKGnMIWUz1_KvT1OxLpoW4YHHg`)
       const data = response.data.imgUrls;
       if (!data || data.length === 0) {
         api.sendMessage("Empty response or no images generated.",event.threadID,event.messageID);
@@ -39,7 +37,7 @@ const response = await axios.get(`https://www.noobs-api.000.pe/dipto/dalle?promp
       }
       await api.unsendMessage(w.messageID);
       await api.sendMessage({
-  body: `𝙨𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡 𝙮𝙤𝙪𝙧 𝙞𝙢𝙖𝙜𝙚 \n\n𝙠𝙝𝙖𝙣 𝙧𝙖𝙝𝙪𝙡 𝙧𝙠💞😘`,
+  body: `𝙨𝙪𝙘𝙚𝙨𝙨𝙛𝙪𝙡 𝙮𝙤𝙪𝙧 𝙞𝙢𝙖𝙜𝙚 \n\n𝙠𝙝𝙖𝙣 𝙧𝙖𝙝𝙪𝙡 𝙧𝙠💞`,
         attachment: diptoo
       },event.threadID, event.messageID);
     } catch (error) {
