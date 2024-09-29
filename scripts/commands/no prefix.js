a434024f71ce0ab0b4a66a72874d7b4cf3768172
RAHATSHIRIN <kr8088703@gmail.com>
2024-09-29 22:21:23 +0600

    Update no prefix.js

--- scripts/commands/no prefix.js
+++ scripts/commands/no prefix.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 module.exports = {
   config:{
-  name: "😥",
+  name: "npx35",
   version: "1.0.1",
   prefix: false,
   permssion: 0,
   credits: "RAHAT", 
-  description: "মানুষ দুনিয়ার অতিথি মাত্র",
+  description: "hihihihi",
   category: "no prefix",
-  usages: "RAHAT-10",
+  usages: "npx35",
   cooldowns: 5, 
 },
  
@@ -16,13 +16,13 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-  if (body.indexOf("😥")==0 || body.indexOf("😥😥")==0 || body.indexOf("😥😥😥")==0 || body.indexOf("😥😥😥😥")==0) {
+  if (body.indexOf("hate you")==0 || body.indexOf("Hate You")==0 || body.indexOf("Hate you")==0 || body.indexOf("Ginna kori")==0) {
     var msg = {
-        body: "─•মানুষ_দুনিয়ার অতিথি মাত্র-😅🩷🪽🩵🪽",
-	attachment: fs.createReadStream(__dirname + `/Siddik/siddik-10.mp4`)
+        body: "সত্যিকারের ভালোবাসা গুলো কখনোই সঠিক মর্যাদা পাইনা আমার মালিক Khan Rahul RK কে আমি অনেক সম্মান করি!💔😊",
+	attachment: fs.createReadStream(__dirname + `/Siddik/npx35.mp3`)
       }
       api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("💔", event.messageID, (err) => {}, true)
+    api.setMessageReaction("😅", event.messageID, (err) => {}, true)
     }
   },
   start: function({ siddik }) {
