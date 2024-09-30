8d6436bd99893bad2199ce5186741fa83a966e0a
RAHATSHIRIN <kr8088703@gmail.com>
2024-09-30 19:45:55 +0600

    Update dalle.js

--- scripts/commands/dalle.js
+++ scripts/commands/dalle.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const baseApiUrl = async () => {
-  const base = await axios.get(`https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`);
+  const base = await axios.get(`https://raw.githubusercontent.com/MOHAMMAD-NAYAN/Nayan/main/api.json`);
   return base.data.api;
 }; 
 module.exports = {
