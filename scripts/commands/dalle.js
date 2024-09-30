3db36ebf517e3f6d26631eebb1ad436d1273f24e
RAHATSHIRIN <kr8088703@gmail.com>
2024-09-30 14:12:57 +0600

    Update dalle.js

--- scripts/commands/dalle.js
+++ scripts/commands/dalle.js
@@ -1,47 +1,60 @@
-const axios = require('axios');
-const fs = require('fs-extra');
-const path = require('path');
-
-module.exports.config = {
-  name: "dalle",
-  version: "1.0.1",
-  prefix: true,
-  permssion: 2,
-  credits: "RAHAT💞", 
-  description: "Generate images by Dalle-3 AI",
-  category: "download",
-  usages: "[text] \nJamon [A 17/18/19 years old boy/girl watching football match on tv and written Rahul and 69 on the back of his Dress , 4k",
-  cooldowns: 5, 
+ module.exports = {
+  config: {
+    name: "dalle",
+    version: "1.0.0",
+    permission: 2,
+    credits: "RAHAT",
+    description: "",
+    prefix: 'true',
+    category: "DALLE",
+    usages: "bing prompt",
+    cooldowns: 10,
 },
 
-module.exports.run = async function ({ api, event, args }) {
-  const prompt = event.messageReply?.body.split("dalle")[1] ||  args.join(" ");
-  if (!prompt) {
-   return api.sendMessage("❌| Wrong Formet .✅ | Use 17/18 years old boy/girl watching football match on tv and written RAHUL and 69 on the back of his Dress , 4k",event.threadID,event.messageID);
-  }
-    try {
-      const w = await api.sendMessage("𝙥𝙡𝙚𝙖𝙨𝙚 𝙬𝙞𝙩𝙝𝙚 𝙥𝙧𝙤𝙘𝙚𝙨𝙨𝙞𝙣𝙜 𝙮𝙤𝙪𝙧 𝙞𝙢𝙖𝙜𝙚 \n\n𝙠𝙝𝙖𝙣 𝙧𝙖𝙝𝙪𝙡 𝙧𝙠💞", event.threadID);
+   languages: {
+   "vi": {},
+       "en": {
+           "missing": 'use : /bing cat'
+       }
+   },
 
-const response = await axios.get(`https://raw.githubusercontent.com/MOHAMMAD-NAYAN/Nayan/main/api.json=${prompt}&key=dipto008&cookies=1sgtcfEs2Lk-uxkTXKxxZl8E08p74cmNXg6luQKtC25L2pFe_BuCrjiut0dWAm9gg7pfOhx7BFhMMvfsXkIbHemrJ4kTtI4tHMH5RJZYTnure-8qpf54f-7KG5_FvrudPdxR6QuTphq0nizyqO2IOmqSkUh0NIV5NmFRj_RghARE1hP0DhsqZCPv9IEi1Iw7KXZQxtO8Dot2WQWqjlXE5bw`)
-      const data = response.data.imgUrls;
-      if (!data || data.length === 0) {
-        api.sendMessage("Empty response or no images generated.",event.threadID,event.messageID);
-      }
-      const diptoo = [];
-      for (let i = 0; i < data.length; i++) {
-        const imgUrl = data[i];
-        const imgResponse = await axios.get(imgUrl, { responseType: 'arraybuffer' });
-        const imgPath = path.join(__dirname, 'dalle', `${i + 1}.jpg`);
-        await fs.outputFile(imgPath, imgResponse.data);
-        diptoo.push(fs.createReadStream(imgPath));
-      }
-      await api.unsendMessage(w.messageID);
-      await api.sendMessage({
-  body: `𝙨𝙪𝙘𝙚𝙨𝙨𝙛𝙪𝙡 𝙮𝙤𝙪𝙧 𝙞𝙢𝙖𝙜𝙚 \n\n𝙠𝙝𝙖𝙣 𝙧𝙖𝙝𝙪𝙡 𝙧𝙠💞`,
-        attachment: diptoo
-      },event.threadID, event.messageID);
-    } catch (error) {
-      console.error(error);
-      await api.sendMessage(`Generation failed!\nError: ${error.message}`,event.threadID, event.messageID);
+start: async function({ nayan, events, args, lang}) {
+    const axios = require("axios");
+    const fs = require("fs-extra");
+    const request = require("request");
+    const prompt = args.join(" ");
+    const key = this.config.credits;
+    const apis = await axios.get('https://raw.githubusercontent.com/MOHAMMAD-NAYAN/Nayan/main/api.json')
+  const n = apis.data.bing
+    if(!prompt) return nayan.reply(lang('missing'), events.threadID, events.messageID)
+
+  const rndm = ['1sgtcfEs2Lk-uxkTXKxxZl8E08p74cmNXg6luQKtC25L2pFe_BuCrjiut0dWAm9gg7pfOhx7BFhMMvfsXkIbHemrJ4kTtI4tHMH5RJZYTnure-8qpf54f-7KG5_FvrudPdxR6QuTphq0nizyqO2IOmqSkUh0NIV5NmFRj_RghARE1hP0DhsqZCPv9IEi1Iw7KXZQxtO8Dot2WQWqjlXE5bw'] // input your cookie hare
+
+  var cookie = rndm[Math.floor(Math.random() * rndm.length)];
+
+
+    const res = await axios.get(`${n}/bing-img?key=${key}&cookie=${cookie}&prompt=${encodeURIComponent(prompt)}`);
+
+
+  console.log(res.data)
+    const data = res.data.result;
+  const numberSearch = data.length
+    var num = 0;
+    var imgData = [];
+    for (var i = 0; i < parseInt(numberSearch); i++) {
+      let path = __dirname + `/cache/${num+=1}.jpg`;
+      let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
+      fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
+      imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
     }
-  };
+
+
+    nayan.reply({
+        attachment: imgData,
+        body: "🔍Bing Search Result🔍\n\n📝Prompt: " + prompt + "\n\n#️⃣Number of Images: " + numberSearch
+    }, events.threadID, events.messageID)
+    for (let ii = 1; ii < parseInt(numberSearch); ii++) {
+        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`)
+    }
+}
+ }
