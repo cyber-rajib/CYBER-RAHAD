d0bfc04f5405d7bde63c66e92a060bd3b1969ecd
RAHATSHIRIN <kr8088703@gmail.com>
2024-09-30 21:38:46 +0600

    Update dalle.js

--- scripts/commands/dalle.js
+++ scripts/commands/dalle.js
@@ -1,35 +1,35 @@
 const axios = require('axios');
 const baseApiUrl = async () => {
-  const base = await axios.get(`https://raw.githubusercontent.com/MOHAMMAD-NAYAN/Nayan/main/api.json`);
+  const base = await axios.get(`https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`);
   return base.data.api;
 }; 
 module.exports = {
   config: {
   name: "dalle",
   version: "1.0",
-  credits: "RAHAT KHAN",
-  hasPermssion: 2,
+  credits: "𝑹𝑨𝑯𝑨𝑻 𝑲𝑯𝑨𝑵",
+  permssion: 0,
   prefix: true,
-  description: "Generate images by Dalle-3 AI",
-  category: "download",
+  description: "𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲 𝗜𝗺𝗮𝗴𝗲𝘀 𝗯𝘆 𝗗𝗮𝗹𝗹𝗲- 𝗔𝗜",
+  category: "𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱",
   usages:
-    "[text] \nJamon [A 17/18/19 years old boy/girl watching football match on tv and written Rahul and 69 on the back of his Dress , 4k]",
+    "[text] \nJamon [A 17/18/19 years old boy/girl watching football match on tv and written 𝑹𝑨𝑯𝑨𝑻 𝑲𝑯𝑨𝑵 and 69 on the back of his Dress , 4k]",
   cooldowns: 5,
 }, 
   run: async({ api, event, args }) => {
     const prompt = (event.messageReply?.body.split("dalle")[1] || args.join(" ")).trim();
-    if (!prompt) return api.sendMessage("❌| Wrong Format. ✅ | Use: 17/18 years old boy/girl watching football match on TV with 'Rahul' and '69' written on the back of their dress, 4k", event.threadID, event.messageID);
+    if (!prompt) return api.sendMessage("❌| Wrong Format. ✅ | Use: 17/18 years old boy/girl watching football match on TV with '𝑹𝑨𝑯𝑨𝑻 𝑲𝑯𝑨𝑵' and '69' written on the back of their dress, 4k", event.threadID, event.messageID);
     try {
        //const cookies = "cookies here (_U value)";
 const cookies = ["1sgtcfEs2Lk-uxkTXKxxZl8E08p74cmNXg6luQKtC25L2pFe_BuCrjiut0dWAm9gg7pfOhx7BFhMMvfsXkIbHemrJ4kTtI4tHMH5RJZYTnure-8qpf54f-7KG5_FvrudPdxR6QuTphq0nizyqO2IOmqSkUh0NIV5NmFRj_RghARE1hP0DhsqZCPv9IEi1Iw7KXZQxtO8Dot2WQWqjlXE5bw"];
 const randomCookie = cookies[Math.floor(Math.random() * cookies.length)];
-      const wait = api.sendMessage("Wait koro baby 😽", event.threadID);
+      const wait = api.sendMessage("𝙥𝙡𝙚𝙖𝙨𝙚 𝙬𝙞𝙩𝙝𝙚 𝙥𝙧𝙤𝙘𝙚𝙨𝙨𝙞𝙣𝙜 𝙮𝙤𝙪𝙧 𝙞𝙢𝙖𝙜𝙚 \n\n𝙠𝙝𝙖𝙣 𝙧𝙖𝙝𝙪𝙡 𝙧𝙠💞", event.threadID);
       const response = await axios.get(`${await baseApiUrl()}/dalle?prompt=${prompt}&key=dipto008&cookies=${randomCookie}`);
 const imageUrls = response.data.imgUrls || [];
       if (!imageUrls.length) return api.sendMessage("Empty response or no images generated.", event.threadID, event.messageID);
       const images = await Promise.all(imageUrls.map(url => axios.get(url, { responseType: 'stream' }).then(res => res.data)));
     api.unsendMessage(wait.messageID);
-   api.sendMessage({ body: `✅ | Here's Your Generated Photo 😘`, attachment: images }, event.threadID, event.messageID);
+   api.sendMessage({ body: `𝙨𝙪𝙘𝙚𝙨𝙨𝙛𝙪𝙡 𝙮𝙤𝙪𝙧 𝙞𝙢𝙖𝙜𝙚 \n\n𝙠𝙝𝙖𝙣 𝙧𝙖𝙝𝙪𝙡 𝙧𝙠💞`, attachment: images }, event.threadID, event.messageID);
     } catch (error) {
       console.error(error);
       api.sendMessage(`Generation failed!\nError: ${error.message}`, event.threadID, event.messageID);
